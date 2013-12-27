8d247fe50ec8117f68ef9b98d6c582f0c773d322
Kei <borgfrog@gmail.com>
2013-12-28 08:47:43 +0900

    Ch 2.3

--- PlayApp_Ch2/app/controllers/Application.java
+++ PlayApp_Ch2/app/controllers/Application.java
@@ -39,4 +39,57 @@ public class Application extends Controller {
 		}
 	}
 
+	public static Result setitem(){
+		Form<Message> f = new Form(Message.class);
+		return ok(item.render("ID番号を入力",f));
+	}
+
+	public static Result edit(){
+		Form<Message> f = new Form(Message.class).bindFromRequest();
+		if (!f.hasErrors()){
+			Message obj = f.get();
+			Long id = obj.id;
+			obj = Message.find.byId(id);
+			if (obj != null) {
+				f = new Form(Message.class).fill(obj);
+				return ok(edit.render("ID = " + id + "の投稿を編集", f));
+			}
+			return ok(edit.render("Edit Error : " + id + " is not found", f));
+		} else {
+			return ok(edit.render("EDIT ERROR : Invalid Input", f));
+		}
+	}
+
+	public static Result update(){
+		Form<Message> f = new Form(Message.class).bindFromRequest();
+		if (!f.hasErrors()){
+			Message data = f.get();
+			data.update();
+			return redirect("/");
+		} else {
+			return badRequest(add.render("UPDATE ERROR", f));
+		}
+	}
+
+	public static Result delete(){
+		Form<Message> f = new Form(Message.class);
+		return ok(delete.render("削除するID番号を入力",f));
+	}
+
+	public static Result remove(){
+		Form<Message> f = new Form(Message.class).bindFromRequest();
+		if (!f.hasErrors()){
+			Message obj = f.get();
+			Long id = obj.id;
+			obj = Message.find.byId(id);
+			if (obj != null) {
+				obj.delete();
+				return redirect("/");
+			}
+			return ok(delete.render("Remove Error : " + id + " is not found", f));
+		} else {
+			return ok(delete.render("REMOVE ERROR : Invalid Input", f));
+		}
+	}
+
 }
