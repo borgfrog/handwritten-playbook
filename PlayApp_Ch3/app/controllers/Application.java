2ccc6b3fb15837045863bcbe745488d8e9660f69
Kei <borgfrog@gmail.com>
2013-12-29 14:52:14 +0900

    Ch 3.3

--- PlayApp_Ch3/app/controllers/Application.java
+++ PlayApp_Ch3/app/controllers/Application.java
@@ -13,11 +13,50 @@ import models.*;
 
 public class Application extends Controller {
 
-	public static Result index(final String msg, final int id) {
-		//return ok("<html><body><h1>Hi!</h1><p>This is test.</p></body></html>").as("text/xml");
-		//return ok(index.render("テンプレートテスト"));
-		//String msg = request().getQueryString("msg");
-		return ok(index.render("Query(msg) - " + msg + ", (id) - " + id));
+	public static class SampleForm {
+		public String input;
+		public String pass;
+		public boolean check;
+		public String radio;
+		public String sel;
+		public String area;
+		public Date date;
+		
+		@Override
+		public String toString() {
+			return "SampleForm [input=" + input + ", pass=" + pass + ", check="
+					+ check + ", radio=" + radio + ", sel=" + sel + ", area="
+					+ area + ", date=" + date + "]";
+		}
 	}
 	
+	public static Result index() {
+		SampleForm sf = new SampleForm();
+		sf.radio = "windows";
+		sf.check = true;
+		sf.input = "default value";
+		sf.sel = "uk";
+		Form<SampleForm> form = new Form(SampleForm.class).fill(sf);
+		return ok(index.render("please set form.", form));
+		/*
+		String method = request().method();
+		if ("GET".equals(method)) {
+			return ok(index.render("please type :"));
+		} else {
+			Map<String, String[]> form = request().body().asFormUrlEncoded();
+			String[] input = form.get("input");
+			return ok(index.render("posted : " + input[0]));
+		}
+		*/
+	}
+	
+	public static Result send() {
+		Form<SampleForm> f = new Form(SampleForm.class).bindFromRequest();
+		if (!f.hasErrors()) {
+			return ok(index.render(f.get().toString(), f));
+		} else {
+			return badRequest(index.render("SEND ERROR", f));
+		}
+	}
+
 }
